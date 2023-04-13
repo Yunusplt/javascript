01e11951a7c54bcb8cc4556d4f48a84fff926d95
yunus <yunuspolat2528@gmail.com>
2023-04-13 17:30:56 +0200

    5th JS Session

--- class-notes/DE-06-Arrays-Methods/arrays.js
+++ class-notes/DE-06-Arrays-Methods/arrays.js
@@ -2,7 +2,7 @@
 // ?                DİZİLER (ARRAYS)
 // ?=========================================================
 
-//* Dizi Tanımlama
+//* Dizi Tanimlama
 //* ---------------------------------------------------------
 
 //! 1. YÖNTEM (Array Literal) - tercih edilen yöntem
@@ -12,7 +12,7 @@ const yaslar=[30,24,55,78,12]
 console.log(yaslar);
 console.log(yaslar.length);
 
-//!hata const keyword ü ile tanımlanmış bir diziyi tamamıyla değiştiremezsiniz, ancak tek tek değişikliğe izin verir
+//!hata const keyword ü ile tanimlanmiş bir diziyi tamamiyla değiştiremezsiniz, ancak tek tek değişikliğe izin verir
 // yaslar=[1,2,3,4]
 
 //! 2. YÖNTEM (Array Constructor)
@@ -20,7 +20,7 @@ console.log(yaslar.length);
 const cars=new Array("BMW", "Mercedes", "Volvo")
 console.log(cars);
 
-//!10 elemanlı bir Array tanımlama
+//!10 elemanli bir Array tanimlama
 const sayilar=new Array(10)    // 10elemanli arrray
 const sayilar1=new Array()  //bos array  
 
@@ -106,7 +106,7 @@ console.log(meyveler);
 meyveler.reverse()
 console.log(meyveler);
 //todo#####################################################
-//! dizinin 3. string elemanını Array e çevirip reverse metodu ile ters dönmüş dizi yaptık. tekrar ters dönmüş dizi olsun istersek join("") kullanabiliriz
+//! dizinin 3. string elemanini Array e çevirip reverse metodu ile ters dönmüş dizi yaptik. tekrar ters dönmüş dizi olsun istersek join("") kullanabiliriz
 console.log(meyveler[4].split("").reverse());
 console.log(meyveler[4].split("").reverse().join(""));
 //todo#####################################################
@@ -114,8 +114,8 @@ console.log(meyveler[4].split("").reverse().join(""));
 
 
 //*splice() 1. parametre dizinin eleman ekleyeceğimiz index ini belirtir
-//*2. parametre=0 ise belirttiğim index teki elemanı sağa ittir, artık orada yeni yazdığım olsun
-//*2.parametre=1 ise belirttiğim index teki elemanı sil üstüne yaz
+//*2. parametre=0 ise belirttiğim index teki elemani sağa ittir, artik orada yeni yazdiğim olsun
+//*2.parametre=1 ise belirttiğim index teki elemani sil üstüne yaz
 
 
 
@@ -128,6 +128,178 @@ meyveler.splice(1,1,"Ananas")   //! meyveler[1]="Ananas"
 console.log(meyveler);
 
 
+
+// todo   #####################################################################################
+// !                           5.DERS
+// todo   #####################################################################################
+
+
+//*sort(), string ifadelerde alfabetik siralar(ascii değerlerine göre)
+//* sort metodu diziyi iterasyona uğratir ve parametre olarak aldiği arrow fonksiyonunu (a-b>0 önce b yi yaz gibi) dizinin her bir elemanina uygular. Bu sayade küçük sayilar ile büyük sayilari yer değişirerek siralama işlemini gerçleştirir.
+
+meyveler.sort()         //! alfabatik sira ascii...
+console.log(meyveler);
+
+const number=[3, 5, 1, 35, 10, 22, 55, 77, 231]
+
+console.log(number);
+number.sort()           //!  [1, 10, 22, 231, 3, 35, 5, 55, 77]  !!!!!!!
+console.log(number); 
+
+
+number.sort((a,b)=>a-b)     //!! [1, 3, 5, 10, 22, 35, 55, 77, 231]  !!!!!!!
+console.log(number);
+
+
+
+//? ===========================================================
+//?  DİZİ ERİŞİM METOTLARI (diziyi değiştirmezler)
+//? ===========================================================
+
+const sayilar2=[3, 5, 2, "2", "uc", 2, "bes", 5]
+
+
+//!  includes() #################################################
+
+
+console.log(sayilar2.includes("5"));   // false
+console.log(sayilar2.includes(5));   //  true
+
+
+//!  indexof()    lastindexof()   ###############################
+
+console.log(sayilar2.indexOf("2"));   // 3
+console.log(sayilar2.indexOf("2", 4));   // -1   4. indexten sonra
+console.log(sayilar2.indexOf(5));   // 1
+console.log(sayilar2.indexOf(5, 2));   // 7     2. indexten sonra
+
+console.log(sayilar2.lastIndexOf(5));  // 7
+
+//todo ############ ORNEK #############
+
+//* kullanicidan sayi isteyin. girilen sayinin hem string hem number hali sayilar dizisinde var mi araştir, varsa index ini döndür.
+
+// const sayilar3 = [3, 5, 2, "2", "uc", 2, "bes", 5, "6"];
+// const sayiString = prompt("lütfen bir sayi giriniz"); //"5"
+
+// const sayiNumber = Number(sayiString); //5
+
+// let varMi=false;
+
+// if (sayilar3.includes(sayiString)) {
+//   console.log(
+//     "aradiğiniz sayinin String hali dizide var ve index i:",
+//     sayilar3.indexOf(sayiString)
+//   );
+//   varMi=true
+// }
+//  else console.log("aradiğiniz sayinin string hali bulunamadi");
+// if (sayilar3.includes(sayiNumber)) {
+//   console.log(
+//     "aradiğiniz sayi dizide var ve index i:",
+// sayilar3.indexOf(sayiNumber)
+//   );
+//   varMi=true
+// }
+
+// else console.log("aradiğiniz sayinin number hali bulunamadi");
+
+// if(varMi==false){
+//   console.log("aradiğiniz sayi bulunamadi");
+// }
+
+
+//!##############################################################################
+
+//* join()
+//*-----------------------------------------------------------
+//? join, dizinin elamanlari birlestirip string hale cevirir.join("x")=>varolan virgül+boşluk sil, elemanlarin aralarina x koy(mesela join parantezinde boşluk yerine 2 varsa her eleman arasina 2 koy demek) 
+//* split(" ")=>string i boşluklardan ayirir,boşluklari silip virgül+boşluk yapar ve yeni dizi döndürür.orijinal diziyi değiştirmez.
+
+//? sayilar2=[3, 5, 2, "2", "uc", 2, "bes", 5]
+
+console.log(sayilar2.join());
+console.log(sayilar2.join(","));
+console.log(sayilar2.join("A "));
+console.log(sayilar2.join("-"));
+
+
+//? ['Ananas', 'Armut', 'Ayva', 'Cilek', 'Erik', 'Mango', 'Muz', 'karpuz']
+
+console.log(meyveler);
+console.log(meyveler[2]);                                // Ayva
+console.log(meyveler[2].split(""));                       //['A', 'y', 'v', 'a']        
+console.log(meyveler[2].split(" "));                       //['Ayva']       
+console.log(meyveler[2].split("").reverse());              //['a', 'v', 'y', 'A'] 
+console.log(meyveler[2].split("").reverse().join(""));     // avyA
+
+
+//!####################################################################################
+
+
+//* toString()
+//*-----------------------------------------------------------
+//? toString fonksiyonu sadece dizinin elemanlarinin aralarina
+//? (virgul) koyarak birlestirir ve string yapar.
+
+console.log(sayilar2);               //? [3, 5, 2, '2', 'uc', 2, 'bes', 5]
+console.log(sayilar2.toString());    //3,5,2,2,uc,2,bes,5 //? bu sekil kullanilir sadece , alir.
+
+
+//! ###################################################################################
+
+//****** */ slice******
+
+const arabalar=["bmw", "mercedes", "audi", "ferrari", "lamborgini"]
+
+console.log(arabalar.slice(3));          //['ferrari', 'lamborgini']
+console.log(arabalar.slice(1, 3));       //['mercedes', 'audi']  start 1 finis 3 
+console.log(arabalar.slice());           // aynisini yazdirir. 5 markada gözükür.
+
+
+//!##################################################################################
+
+//***** */ concat****/
+
+const kisiler=["özlem","esra", "nihal", "fatih","hüseyin"]
+const rakamlar=[1,2,3,4,5,6]
+
+const birlesik=kisiler.concat(rakamlar,true, "halit","emirhan", ["irfan","yunus"], [["a","b","c"]])
+
+console.log(birlesik);   //?['özlem', 'esra', 'nihal', 'fatih', 'hüseyin', 1, 2, 3, 4, 5, 6, true, 'halit', 'emirhan', 'irfan', 'yunus', Array(3)]
+
+
+//!##############################################################################
+
+//* every()
+//*-----------------------------------------------------------
+//? Tum diziyi itere eder ve aldigi callback fonksiyonuna gore
+//? test gerceklestirir.Tum elemanlar icin test basarili ise
+//? true aksi takdirde false deger dondurur.
+
+const yas=[18, 23, 35, 44, 57, 89, 15]
+
+console.log(yas.every((x)=> x>=10));             //true cünkü tüm elemanlar 10 dan büyük.
+console.log(yas.every((x)=> x>=18));             // false cünkü tem elemanlar 18 den büyük degil.
+console.log(yas.every((x)=> x>=100));             // false  
+
+
+
+//!##############################################################################
+
+//* some()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna (bizim yazdığımız fonksiyon) gore test gerceklestirir.
+//? En az bir eleman icin bile test basarili ise true aksi
+//? takdirde false deger dondurur.
+
+
+console.log(yas.some((x)=> x>=10));         //true       // 1 tane bile sart tutarsa true
+console.log(yas.some((x)=> x>=40));         // true 
+console.log(yas.some((x)=> x>=100));        // false
+
+
+//!################################################################################
 
 
 
---
+++ class-notes/DE-07-Arrays-iteration/1-array-loop.js
@@ -0,0 +1,159 @@
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//*                       ITERATION IN ARRAYS
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//* ======================================================
+//*                     FOR LOOP
+//* ======================================================
+//*ÖRNEK: dizi1 arrayindeki pozitif sayılarla negatif sayıları 2 ayrı array de toplayan function yazınız
+
+//!##################################################################################
+//******************** */ with i*********************** 
+
+const dizi1 = [-5, 15, 22, -4, 45, 79, -54];
+
+const negatifler1 = [];
+const pozitifler1 = [];
+
+const dizilereAyir1 = () => {
+  for (let i = 0; i < dizi1.length; i++) {
+    if (dizi1[i] < 0) {
+      negatifler1.push(dizi1[i]);
+    } else pozitifler1.push(dizi1[i]);
+  }
+
+  console.log(negatifler1);
+  console.log(pozitifler1);
+};
+
+dizilereAyir1();
+
+
+//!###################################################################################
+
+//************ */  FOR IN  ************
+
+const dizi2 = [-5, 15, 22, -4, 45, 79, -54];
+
+const negatifler2 = [];
+const pozitifler2 = [];
+
+const dizilereAyir2 = () => {
+  for (let i in dizi2) {
+    if (dizi2[i] < 0) {
+      negatifler2.push(dizi2[i]);
+    } else pozitifler2.push(dizi2[i]);
+  }
+
+  console.log(negatifler2);                  //  [-5, -4, -54]
+  console.log(pozitifler2);                 // [15, 22, 45, 79]
+};
+
+dizilereAyir2();
+
+
+
+
+//!####################################################################################
+
+//********* */ FOR OF********
+
+const dizi3 = [-5, 15, 22, -4, 45, 79, -54];
+
+const negatifler3 = [];
+const pozitifler3 = [];
+
+const dizilereAyir3 = () => {
+  for (let item of dizi3) {
+    if (item < 0) {
+      negatifler3.push(item);
+    } else pozitifler3.push(item);
+  }
+
+  console.log(negatifler3);
+  console.log(pozitifler3);
+};
+
+dizilereAyir3();
+
+
+
+
+//!####################################################################################
+
+
+//* ======================================================
+//*                   FOR IN LOOP
+//* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+//* bir dizide saklandığını varsayalım.Bu hayvanları türüne
+//* göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
+//*  ana programa döndürecek fonksiyonu yazınız.
+//*--------------------------------------------------------
+
+
+const animal = [
+  "fil",
+  "aslan",
+  "deve",
+  "fil",
+  "kaplan",
+  "fil",
+  "deve",
+  "aslan",
+  "aslan",
+  "aslan",
+];
+
+
+
+// const userAnimal = prompt("lütfen bir hayvan adı giriniz");
+
+const findAnimal = () => {
+  let sayac = 0;
+
+  for (let i in animal) {
+    if (animal[i] == userAnimal) sayac++;
+  }
+
+  return ` ${userAnimal} hayvanindan  ${sayac} adet bulunmaktadir`;
+};
+
+// alert(findAnimal());
+
+
+
+//!###############################################################################
+
+//* ======================================================
+//*                   FOR OF LOOP
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+
+//* ORNEK: alttaki dizinin elemanlarını yanyana string olarak yazdır (for of döngüsü kullanın)
+
+
+
+const arabalar = ["bmw", "mercedes", "audi", "volvo"];
+
+let araba=""
+
+for(let item of arabalar){
+
+araba+=item
+}
+console.log(araba);            //bmwmercedesaudivolvo
+
+
+
+//! 2.yol
+// const araba = arabalar.join("");
+// console.log(araba);
+
+
+
+
+
