42372c335c430134bfcaa2be62dd0e830de711bf
yunus <yunuspolat2528@gmail.com>
2023-04-19 12:32:02 +0200

    dom_1 finished

---
+++ class-notes/DE-10-Dom-Intro/1-selector.js
@@ -0,0 +1,124 @@
+//* ======================================================
+//*                   getElementById()
+//* ======================================================
+
+
+//! example_1 (button style) ##################################################
+
+const button = document.getElementById("btn")
+
+button.style.width = "200px"
+button.style.height = "80px"
+button.style.fontSize = "40px"
+button.style.color = "yellow"
+button.style.backgroundColor = "red"
+
+
+//! example_2 (pragraph style) ##################################################
+
+const paragraf = document.getElementById("par")
+
+paragraf.style.backgroundColor = "gray"
+paragraf.style.color = "orange"
+paragraf.style.fontSize = "50px"
+paragraf.style.padding = "5px"
+
+//!##############################################################################
+
+
+//* =============================================================================
+//*                   getElementByTAGNAME()
+//* tag ismiyle çağırıyoruz.aynı tag den çok olacağı için index le hangisini seçtiğimizi belirtiyoruz (HTMLCollection- Array özellikleri gösterir)
+//*=============================================================================
+
+
+//! example_3  (img style) ######################################################
+
+const resim = document.getElementsByTagName("img")
+
+console.log(resim);                  // HTML Collection 
+
+resim[0].style.width = "300px"
+resim[1].style.border = "5px solid red"
+
+//!##############################################################################
+
+
+//* ======================================================
+//*                   getElementByCLASSNAME()
+//* class ismiyle çağırıyoruz
+//*======================================================
+
+
+const baslik = document.getElementsByClassName("H1")
+
+console.log(baslik);                //HTML Collection
+
+baslik[0].style.color = "red"
+baslik[1].style.textAlign = "center"
+
+//! array'i degistiren methodlari kullanabiliriz.
+
+for (let i=0 ; i < baslik.length; i++){
+
+    baslik[i].style.color = "purple"
+    baslik[i].style.fontSize = "50px"
+
+}
+
+//!######################################################################
+
+baslik[0].innerHTML=" <a href='https://www.clarusway.com' >DOM COK KOLAY</a> "
+//todo DOM SELECTOR yazan h1 etiketini, DOM COK KOLAY yazan <a> etiketine dönusturduk
+
+// baslik[1].innerHTML="NAPIYORSUN"
+
+baslik[1].textContent = "NASILSIN"
+//todo sadece içerik değişecekse textContent tercih edilir, ama innerHTML de etiketi değiştirebildiği gibi, içeriği de değiştirebilir
+
+//!###################################################################################
+
+//* ===================================================================================
+//*                  QUERYSELECTOR,QUERYSELECTORALL
+//*====================================================================================
+
+document.querySelector("title").textContent="DOM MANiPULATION 💜"
+document.querySelector("#badi").style.backgroundImage="linear-gradient(green, pink)"
+document.querySelector(".H2").style.color="yellow"
+//todo birden fazla h2 olsa ilkini alirdi. 
+document.querySelector("p")     //! ilk p getirir.
+document.querySelectorAll("p")  //! tüm p ler geldi. 
+console.log(document.querySelectorAll("p"));            // NodeList
+
+document.querySelectorAll("p")[1].style.fontSize = "70px"
+
+
+//!###################################################################################
+
+
+// Bir NodeList ve bir HTMLcollection hemen hemen aynı şeydir.
+
+// Her ikisi de bir belgeden çıkarılan düğümlerin (öğelerin) dizi benzeri koleksiyonlarıdır (listeleridir). Düğümlere indeks numaraları ile erişilebilir. indeks 0'dan başlar.
+
+// Her ikisinin de listedeki (koleksiyon) öğelerin sayısını döndüren bir uzunluk özelliği (length) vardır.
+
+
+// HTMLCollection öğelerine adları, kimlikleri veya dizin numaralarıyla erişilebilir.
+// baslik.namedItem("id1").style.color = "red";
+//  baslik[0].style.color="red";
+
+// NodeList öğelerine yalnızca dizin numaralarıyla erişilebilir.
+
+// Bir HTMLCollection her zaman canlı bir koleksiyondur. Örnek: DOM'daki bir listeye bir <li> öğesi eklerseniz, HTMLCollection'daki liste de değişecektir.
+
+// Bir NodeList genellikle statik bir koleksiyondur. Örnek: DOM'daki bir listeye bir <li> öğesi eklerseniz, NodeList'teki liste değişmez.
+// getElementsByClassName()ve yöntemleri , getElementsByTagName() canlı bir HTMLCollection döndürür.
+
+// querySelectorAll(), statik bir NodeList döndürür.
+//!burada netten https://softauthor.com/javascript-htmlcollection-vs-nodelist/ adresinden göster
+// Dizi Değil!
+// Bir NodeList bir dizi gibi görünebilir ama öyle değildir.
+
+// Bir NodeList içinde döngü yapabilir ve düğümlerine dizine göre başvurabilirsiniz.
+
+// Ancak, bir NodeList'te push(), pop() veya join() gibi Array yöntemlerini kullanamazsınız.
---
+++ class-notes/DE-10-Dom-Intro/2-events.js
@@ -0,0 +1,219 @@
+//* ======================================================
+//*                        EVENTS
+//* ======================================================
+//!#################################################################################
+//* EXAMPLE-1 (Mouse Over, Mouse Out )
+//* -------------------------------------------------------
+
+//?METHOD-1 (HTML-INLINE)
+//?---------------------------------------------------------------
+
+//todo alltaki satirlar HTML'de yazildi.
+
+    // <h1 
+    // onmouseover="style.color='blue'"
+    // onmouseout="style.color='red'"
+    // >JAVASCRIPT'TE DOM</h1>
+
+
+
+
+//? METHOD-2 (Function calling in HTML)
+//?---------------------------------------------------------------
+//todo alltaki satirlar HTML'de yazildi. iki alt satirdakileri uygulamak icin 
+
+    // <h1 class="H5" 
+    // onmouseover = "yesil()"
+    // onmouseout="kirmizi()"
+    // >DOM BASICS</h1>
+
+// JAVASCRIPT
+const baslik = document.querySelector(".H5")
+
+const yesil =()=>baslik.style.color = "green"
+
+// yesil()   //! yesil fonksiyonu cagirildigi zaman yesil yapar. 
+
+const kirmizi = () => baslik.style.color = "orange"
+
+// kirmizi()
+
+//!#################################################################################
+
+//? METHOD-3 (SADECE JAVASCRİPT TE YAZILAN YOL) 1.yol
+//?---------------------------------------------------------------
+
+//* Example_1 **********************************************************************
+
+document.querySelector("#js").onmouseover=()=>{
+
+    document.querySelector("#badi").style.backgroundColor="pink"
+    document.querySelector("#js").style.color="purple"
+    
+}
+
+document.querySelector("#js").onmouseout=()=>{
+
+    document.querySelector("#badi").style.backgroundColor="red"
+    document.querySelector("#js").style.color="yellow"
+    
+}
+
+//todo hocanin kisaltma yapilmis cozümü asagidadir.
+
+// const js = document.querySelector("#js");
+// const badi = document.querySelector("#badi");
+// js.onmouseover = () => {
+//   badi.style.backgroundColor = "pink";
+// };
+// js.onmouseout = () => {
+//   badi.style.backgroundColor = "red";
+// };
+
+//!##################################################################################
+
+//* Example_2 (onclick-ondblclick)*************************************************
+
+
+const birinci = document.querySelector(".bir")
+const ikinci = document.querySelector(".iki")
+
+// birinci.onclick=()=>{
+
+//     birinci.src="./img/logo2.png"
+//     ikinci.src="./img/js_logo.png"
+
+// }
+
+// ikinci.onclick=()=>{
+
+//     birinci.src="./img/js_logo.png"
+//     ikinci.src="./img/logo2.png"
+
+// }
+//! neden yukardakini kullanmadik. cünkü yukardakinde 1. olmadan 2. olmasi mantikli degil. bunu aktiflestirmek icin 1. olduktan sonra 2. olmali dedik ve bunu algoritmaya doktuk. 
+//! 1.yol ########################
+birinci.onclick=()=>{
+
+    birinci.src="./img/logo2.png"
+    ikinci.src="./img/js_logo.png"
+    icerde()
+}
+
+const icerde = () => {
+    ikinci.onclick=()=>{
+
+    birinci.src="./img/js_logo.png"
+    ikinci.src="./img/logo2.png"
+    }
+}
+
+//! 2.yol #########################
+// birinci.onclick=()=>{
+
+//     birinci.src="./img/logo2.png"
+//     ikinci.src="./img/js_logo.png"
+
+//     ikinci.onclick=()=>{
+
+//     birinci.src="./img/js_logo.png"
+//     ikinci.src="./img/logo2.png"
+
+// }
+// }
+
+//!##################################
+
+ikinci.ondblclick=()=>{
+
+    birinci.src = "./img/bir.png"
+    ikinci.src = "./img/iki.png"
+
+
+}
+
+
+//! ###############################
+
+//* example_3 *********************
+
+const buton = document.querySelector("#btn")
+
+buton.onclick=()=>{
+
+    document.querySelector("#badi").style.backgroundImage="linear-gradient(to right, purple, gray"
+
+    buton.textContent="SEARCH"
+    buton.style.fontSize="40px"
+    buton.style.color="red"
+
+
+}
+
+
+//!aynı element e (merhaba yazısına mouse ile gelince badi renk değiştirdin demiştik) hem onclick hem onmouseover atandığında, önce onmouseover  sonra onclick i çalıştırırsanız sorun olmaz, tersi durumda onclick baskın old için onmouseover çalışmaz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
