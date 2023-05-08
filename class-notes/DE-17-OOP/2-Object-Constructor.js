73a0ad086e04e68989ef36ed3268597b0e4afab6
yunus <yunuspolat2528@gmail.com>
2023-05-08 21:18:42 +0200

    OOP

---
+++ class-notes/DE-17-OOP/2-Object-Constructor.js
@@ -0,0 +1,78 @@
+//* ======================================================
+//*          OOPS- Object Constructor (ES5)
+//* ======================================================
+
+//*Object Constructor
+//* Javascript is a prototype-based language.
+//* Tüm JavaScript nesneleri, bir prototipten (ilk örnek)özellikleri ve yöntemleri devralır (inherit)
+//* Object prototipi, prototip miras zincirinin en üstündedir.(Hz. Adem)
+//* For example, Date and Array objects,   Object prototype den devralır
+
+//* Object Constructor
+//! Book ismi büyük harfle,constructor olduğu biz tarafından anlaşılsın diye. anne karnında bebek var doğarsa değeri olur (üstte constructor anne karnındaki bebek, altta book1 oluşturmak=bebeğin doğması)
+//!arrow function this keyword unu desteklemediği için burada function decleration yazımını tercih ettik
+
+
+function Book(a,b,c) {
+    this.author=b,
+    this.title=a,
+    this.year=c,
+    this.summaryFunction = function () {
+        return `${this.title} kitabini ${this.year} yilinda ${this.author} yazmistir.`;
+    }
+}
+
+//? new keyword ü  Book Constructor ı parametrelerle çağırır.
+//? Constructor, Book object in bir örneğini kalıbını oluşturur.
+//? Constructor daki tüm variablelar ve functionlar, oluşturulan her  single örneğe (instance) eklenir
+
+const book1 = new Book("Karamazov kardesler", "Dostyevski", 1980)
+const book2 = new Book("Nutuk", "Atatürk", 1925)
+
+console.log(book1);
+console.log(book2.summaryFunction());
+
+//!kalıp ta prototype alanına ulaşmak için alttaki gibi bir syntax kullanılır
+//*Book un prototype alanına yeni bir parametre ekledik ama bellekte yer kaplamaz, ihtiyaç olunca child lar kullanır
+
+Book.prototype.type="roman"
+Book.prototype.findYear = function () {
+    return 2023 - this.year
+}
+
+//?prototype book2 de yer kaplamadı ama çağırınca geldi
+console.log(book2.findYear());
+console.log(book1.tür);
+
+//*__proto__ keywordüyle yapılan değişiklik hala arka planda durur ve bellekte yer kaplamaz, birinin proto sunda yapılan değişiklik her child ı etkiler
+book2.__proto__.tür="hikaye"            //todo güncelleme
+console.log(book2.tür);
+console.log(book2);
+
+
+//?direk object lerime açıktan bir value eklemek istersem, bellekte yer kaplar, alttaki gibi yazılır
+book1.price = 100                      //todo sonradan key cifti girmek
+console.log(book1);
+
+
+
+
+
+//todo ##################### DERS _ 2 #######################################
+
+//!OKUL YÖNETİMİNİ HATIRLA
+//? INHERITANCE
+
+function Dergi(title, author, year, d) {
+    Book.call(this,title, author, year)
+    this.mounth = d
+}
+
+//*Book object inden (kalıbından) türetilen Dergi kalıbına, Book un prototype alanındaki bilgiler (ES5 te) direk gelmez,gelmesini istiyorsak alttaki kodu (daha child oluşturmadan önce)yazmalıyız
+Dergi.prototype = Object.create(Book.prototype)
+
+const dergi1 = new Dergi("yaprak dokumu", "resut nuri", 1980, "eylül")
+
+console.log(dergi1);
+console.log(dergi1.summaryFunction());
+
---
+++ class-notes/DE-17-OOP/3-classes-ES6.js
@@ -0,0 +1,55 @@
+//* ======================================================
+//*             OOP - Classes (ES6)
+//* ======================================================
+
+//? Classlar,  objects oluşturmak için template (şablondur).
+//? JS deki Classes,   prototypes üzerine kuruludur,  ayni zamanda ES5 syntax indan farkli bazi syntax and semantics e sahiptir .
+//? Aslinda  class keyword ü  ES6 da tanitilmiştir, but is syntactical sugar (yalnizca dili kullanmanin bazi yönlerini daha kolay ve genel olarak daha keyifli hale getirmek için eklenen bir dil özelliği için kullanilan bir terimdir.), JavaScript  prototype-based dir.
+
+
+class Book {
+  constructor(a, b, c) {
+    this.title = a;
+    (this.author = b), (this.year = c);
+    this.özetFunction = function () {
+      return `${this.title} kitabini ${this.year} yilinda ${this.author} yazmiştir`;
+    };
+  }
+  //?bu alt süslüye yazdiklarimiz direk kalibin prototype alanina gider (ES6 ile biz belirtmeden)
+  yilHesapla() {
+    return 2023 - this.year;
+  }
+}
+
+const book1 = new Book("safahat", "M.Akif Ersoy", 1910);
+
+console.log(book1);
+console.log(book1.yilHesapla());
+
+//?INHERITANCE
+
+//*Book object kalibindan Dergi object ine kalip getir
+
+class Dergi extends Book {
+  constructor(author, title, year, d) {
+    super(author, title, year);
+
+    this.mounth = d;
+  }
+}
+
+const dergi1 = new Dergi("kasagi", "Omer Seyfettin", 1932, "aralik");
+
+console.log(dergi1);
+console.log(dergi1.yilHesapla());
+
+class Gazete extends Book {
+  constructor(author, title, year, d, e) {
+    super(author, title, year);
+    this.mounth = d;
+    this.fiyat = e;
+  }
+}
+const f = new Gazete("kasagi", "", 1932, "aralik", 19);
+console.log(f);
+//! extends edildiği zaman, atadan gelen her veriyi kullanmak zorundayiz, en fazla value kismini boş " " birakabiliriz
